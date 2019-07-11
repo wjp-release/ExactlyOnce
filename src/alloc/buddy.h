ace3febb9ef1ec98b9d92f0775e9d4907172c638
root <root@DESKTOP-07C9913.localdomain>
2019-07-11 21:15:23 +0800

    auto commit

---
+++ src/alloc/buddy.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "common.h"
+
+namespace wjp{
+
+struct BuddyBlock{
+
+};
+
+class BuddySystem{
+public:
+
+
+
+private:
+    // 合并互为buddy的A、B，返回合并后的块
+    BuddyBlock*     merge(BuddyBlock* A, BuddyBlock* B){
+
+    }
+
+    void
+
+};
+
+
+
+}
--- src/util/priorityq.h
+++ src/util/priorityq.h
@@ -26,27 +26,79 @@ public:
 
     void heapify(int index = 0){
         int root = index;
-        if( left(index) >= arr.size()) return;
-
+        if (left(index) >= arr.size()) return;
+        if (less(left(index), root)) root = left(index);
+        if (less(right(index), root)) root = right(index);
+        if (root != index){
+            swaparr(index, root);
+            heapify(root);
+        }
     }
 
-    void push(){
-
-
+    void push(const ValueType& value){
+        arr.push_back(value);        
+        insert(arr.size()-1, 0, value);
     }
 
     ValueType pop(){
-
+        if (arr.size() == 0) throw std::runtime_error("heap empty");
+        auto duh = arr[0];
+        arr[0] = arr.back();
+        arr.pop_back();
+        heapify(0);
+        return duh;
     }
 
-    void update(int index, ValueType newval){
-
+    void update(int index, const ValueType& newval){
+        auto oldval = arr[index];
+        arr[index] = newval;
+        if (lessPredicate(newval, oldval)){
+            heapify(index);
+        }else{
+            insert(index, 0, newval);
+        }
     }
+
+    void makeHeap(){
+        if (arr.size() < 2) return;
+        if (arr.size() == 2){
+            if (less(1,0)) swaparr(0, 1);
+            return;
+        }
+        size_t last = arr.size() - 1;
+        for (int p = parent(last); p >= 0; p--) heapify(p);
+    }
+
+    bool isHeapUntil(){
+        size_t p = 0;
+        size_t n = arr.size();
+        for (size_t child = 1; child < n; ++child){
+            if (less(child, p)) return child;
+            if ((child & 1) == 0) ++p;
+        }
+        return n;
+    }
+
+    inline size_t size() { return arr.size(); }
+
+    inline bool empty() { return arr.empty(); }
 
 private:
     static inline int left(int i) { return (i << 1) + 1; }
     static inline int right(int i) { return (i << 1) + 2; }
     static inline int parent(int i) { return (i - 1) >> 1; }
+
+    inline bool less(size_t id1, size_t id2){
+        return lessPredicate(arr[id1], arr[id2]);
+    }
+
+    inline bool equal(size_t id1, size_t id2){
+        return !less(id1, id2) && !less(id2, id1);
+    }
+
+    inline void swaparr(size_t a, size_t b){
+        std::swap(arr[a], arr[b]);
+    }
 
     LessPredicate lessPredicate;
     std::vector<ValueType> arr;
